35b14f5feabc1ef8bfea10f04228a42de9bd2d1b
EddyerDevv <urbinaramalazo1986@gmail.com>
2025-03-23 01:50:34 -0600

    feat(ui): added a basic footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<footer className="mx-8 mt-auto flex flex-col items-center justify-start pt-[2rem] pb-[1rem] w-full">
+			<div className="flex flex-row flex-wrap justify-between gap-x-8 gap-y-4 text-[0.7rem] text-black/50">
+				<p>Copyright © 2024 - 2025 EddyDevv. Powered by Optimistic Studios.</p>
+			</div>
+		</footer>
+	);
+}
