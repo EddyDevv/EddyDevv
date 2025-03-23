5f2c278c7c445b1890a787cdc719a0dd41d5fa6f
EddyerDevv <urbinaramalazo1986@gmail.com>
2025-03-23 01:50:10 -0600

    feat(ui): added the skills and technologies section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 
 export default function Home() {
 	return (
@@ -7,6 +11,9 @@ export default function Home() {
 			<section className="w-[100%] max-w-[64rem] min-h-dvh flex items-center  px-3 flex-col gap-2 pb-4">
 				<Header />
 				<Hero />
+				<Projects />
+				<Skills />
+				<Footer />
 			</section>
 		</main>
 	);
